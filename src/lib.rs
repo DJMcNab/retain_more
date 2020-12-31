942e77f2f8be2818feae14fc21eb47c25d070892
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2020-12-31 17:43:38 +0000

    Remove SafeImpl
    It was never going to be used

--- src/lib.rs
+++ src/lib.rs
@@ -3,30 +3,4 @@ extern crate alloc;
 
 mod string;
 
-use core::ops::{Deref, DerefMut};
-
 pub use string::RetainMoreString;
-
-/// A wrapper type which implements the traits safely
-#[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Hash, Ord, PartialOrd)]
-pub struct SafeImpl<T>(pub T);
-
-impl<T> From<T> for SafeImpl<T> {
-    fn from(it: T) -> Self {
-        Self(it)
-    }
-}
-
-impl<T> DerefMut for SafeImpl<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl<T> Deref for SafeImpl<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
